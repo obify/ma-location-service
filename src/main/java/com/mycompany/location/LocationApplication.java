edbc608e368c254748de9b0fa05ab70cd04eb4de
Pabitra Panda <pk.pabitra18@gmail.com>
2021-07-21 01:50:47 +0530

    initial commit ma-location-service

---
+++ src/main/java/com/mycompany/location/LocationApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationApplication.class, args);
+	}
+
+}
